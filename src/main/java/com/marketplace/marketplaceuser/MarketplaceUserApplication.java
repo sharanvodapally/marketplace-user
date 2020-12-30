a438085060a02bd6a2804e3b02c68be4a51cd376
sharanvodapally <sharanvodapally@sharans-mbp.verizon.net>
2020-12-29 21:53:11 -0500

    initial commit

---
+++ src/main/java/com/marketplace/marketplaceuser/MarketplaceUserApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.marketplaceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketplace/marketplaceuser/MarketplaceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.marketplaceuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
