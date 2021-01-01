fe268feb8124c921e2b570d6a6bc9cdb720eb486
sharanvodapally <sharanvodapally@sharans-mbp.verizon.net>
2020-12-31 23:46:06 -0500

    user s

--- src/main/java/com/marketplace/marketplaceuser/MarketplaceUserApplication.java
+++ src/main/java/com/marketplace/marketplaceuser/MarketplaceUserApplication.java
@@ -2,8 +2,10 @@ package com.marketplace.marketplaceuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MarketplaceUserApplication {
 
 	public static void main(String[] args) {
